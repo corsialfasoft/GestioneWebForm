c9e1328d73568478c20f6a8486312a196e0372c7
TandyD <brinzila30@gmail.com>
2018-04-19 16:45:28 +0200

    Creazione db

---
+++ GestioneWebForm/DB/CreazioneDBGeCo.sql
@@ -0,0 +1,28 @@
+﻿create database GeCo;
+
+use GeCo;
+create table Corsi(
+	id int identity(1,1) primary key not null,
+	nome nvarchar(50) not null,
+	descrizione nvarchar (200),
+	dInizio date,
+	dFine date
+);
+
+create table Lezioni (
+	id int identity(1,1) primary key not null,
+	durata nvarchar(50) not null,
+	idCorsi int foreign key references Corsi
+);
+
+create table Studenti(
+	matr nvarchar(10) primary key not null,
+	nome varchar (15) not null,
+	cognome varchar(15) not null
+);
+
+create table StudentiCorsi(
+	idCorsi int foreign key references Corsi,
+	idStudenti nvarchar(10) foreign key references Studenti,
+	primary key(idCorsi,idStudenti)
+);
