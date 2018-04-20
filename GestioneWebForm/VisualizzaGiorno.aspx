c160d7bd84584b5f82682e8198146743cca256cd
JokerJackassJam <edo.bevione@gmail.com>
2018-04-20 13:53:27 +0200

    VisualizzaGiorno con Mock terminato e funzionante

--- GestioneWebForm/VisualizzaGiorno.aspx
+++ GestioneWebForm/VisualizzaGiorno.aspx
@@ -1,7 +1,7 @@
-﻿<%@ Page Title="Dettagli del Giorno" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="VisualizzaGiorno.aspx.cs" Inherits="GestioneWebForm._VisualizzaGiorno" %>
+﻿<%@ Page Title="Registro Giorni" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="VisualizzaGiorno.aspx.cs" Inherits="GestioneWebForm._VisualizzaGiorno" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
+    <h2><%: Title %></h2>
     <br />
     <div class="text-warning" style="font-size:large">
         <%=Message %>
@@ -13,20 +13,12 @@
     </div>
     <br>
     <%if (giorno!=null && giorno.Data.ToString("yyyy-MM-dd") != "0001-01-01") { %>
+        <div>
+            <h2>Dettagli del giorno selezionato:</h2>
+        </div>
         <div class="panel panel-default">
             <div class="panel-heading"><%=giorno.Data.ToShortDateString()%></div>
-            <div class="table">
-                <table style="font-family: arial, sans-serif; border-collapse: collapse; width: 100%; text-align:center;">
-                    <tr>
-                        <td>Tipo</td>
-                        <td>Totale ore</td>
-                        <td>Commessa</td>
-                        <td>Ore lavoro commessa</td>
-                        <td>Note</td>
-                    </tr>
-                </table>
-                <asp:Table ID="TabellaGiorno" runat="server" width="100%" CellPadding="10" GridLines="None" HorizontalAlign="Center"></asp:Table>
-            </div>
+            <asp:Table ID="TabellaGiorno" runat="server" width="100%" CellPadding="10" GridLines="None" HorizontalAlign="Center" CssClass="table"></asp:Table>
         </div>
     <%} %>
 </asp:Content>
--- GestioneWebForm/VisualizzaGiorno.aspx.cs
+++ GestioneWebForm/VisualizzaGiorno.aspx.cs
@@ -16,23 +16,64 @@ namespace GestioneWebForm {
         }
 
         protected void SelezionaGiorno(object sender,EventArgs e) {
+            int count=0;
             if(IsPostBack) {
                 giorno = new DataAccesObject().VisualizzaGiorno(data.SelectedDate, "admin");
                 if (giorno == null) {
                     Message= "La data selezionata non è presente nel DB!";
                 } else if (giorno!=null && giorno.Data.ToString("yyyy-MM-dd") != "0001-01-01") {
-                    //TabellaGiorno.Cancellatutto!!!
+                    if (TabellaGiorno!=null) {
+                       TabellaGiorno.Rows.Clear();
+                    }
                     TableRow tr = new TableRow();
+                    TableCell tdDescr = new TableCell();
+                    tdDescr.Controls.Add(new Label() { Text = "Tipo", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2", });
+                    tdDescr.Controls.Add(new Label() { Text = "Ore Totali", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2", });
+                    tdDescr.Controls.Add(new Label() { Text = "Commessa", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2", });
+                    tdDescr.Controls.Add(new Label() { Text = "Ore Lavoro Commessa", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2", });
+                    tdDescr.Controls.Add(new Label() { Text = "Note", CssClass="col-xs-4 col-sm-4 col-md-4 col-lg-4", });
+                    tr.Cells.Add(tdDescr);
+                    TabellaGiorno.Rows.Add(tr);
+
+                    tr = new TableRow();
                     TableCell tdOreLavoro = new TableCell();
-                    tdOreLavoro.Controls.Add(new Label() {Text = "Ore di lavoro", CssClass="col-xs-2"});
-                    tdOreLavoro.Controls.Add(new Label() { Text = giorno.TotOreLavorate().ToString(), CssClass="col-xs-2" });
+                    tdOreLavoro.Controls.Add(new Label() {Text = "Ore di lavoro", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2"});
+                    tdOreLavoro.Controls.Add(new Label() { Text = giorno.TotOreLavorate().ToString(), CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                    foreach(OreCommessa ocomm in giorno.OreLavorate) {
+                        if (count>0) {
+                            tr = new TableRow();
+                            tdOreLavoro.Controls.Add(new Label() { CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                            tdOreLavoro.Controls.Add(new Label() { CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                        }
+                        tdOreLavoro.Controls.Add(new Label() { Text = giorno.OreLavorate[count].Nome.ToString(),CssClass = "col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                        tdOreLavoro.Controls.Add(new Label() { Text = giorno.OreLavorate[count].Ore.ToString(),CssClass = "col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                        tdOreLavoro.Controls.Add(new Label() { Text = giorno.OreLavorate[count].Descrizione.ToString(),CssClass = "col-xs-4 col-sm-4 col-md-4 col-lg-4" });
+                        tr.Cells.Add(tdOreLavoro);
+                        TabellaGiorno.Rows.Add(tr);
+                        count++;
+                    }
                     tr.Cells.Add(tdOreLavoro);
                     TabellaGiorno.Rows.Add(tr);
+
                     tr = new TableRow();
                     TableCell tdOrePerm = new TableCell();
-                    tdOrePerm.Controls.Add(new Label() { Text = "Ore di Permesso", CssClass="col-xs-2" });
-                    tdOrePerm.Controls.Add(new Label() { Text = giorno.HPermesso.ToString(), CssClass="col-xs-2" });
+                    tdOrePerm.Controls.Add(new Label() { Text = "Ore di Permesso", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2", });
+                    tdOrePerm.Controls.Add(new Label() { Text = giorno.HPermesso.ToString(), CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
                     tr.Cells.Add(tdOrePerm);
+                    TabellaGiorno.Rows.Add(tr);
+
+                    tr = new TableRow();
+                    TableCell tdOreMal = new TableCell();
+                    tdOreMal.Controls.Add(new Label() { Text = "Ore di Malattia", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                    tdOreMal.Controls.Add(new Label() { Text = giorno.HMalattia.ToString(), CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                    tr.Cells.Add(tdOreMal);
+                    TabellaGiorno.Rows.Add(tr);
+
+                    tr = new TableRow();
+                    TableCell tdOreFerie = new TableCell();
+                    tdOreFerie.Controls.Add(new Label() { Text = "Ore di Ferie", CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                    tdOreFerie.Controls.Add(new Label() { Text = giorno.HFerie.ToString(), CssClass="col-xs-2 col-sm-2 col-md-2 col-lg-2" });
+                    tr.Cells.Add(tdOreFerie);
                     TabellaGiorno.Rows.Add(tr);
                 }
             }
